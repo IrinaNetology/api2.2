c0c7e95280669d735c0dfac82eee739dbd3a9d90
Irina <irakali86@gmail.com>
2023-01-17 13:35:34 +0300

    Initial commit

---
+++ src/test/java/ru/netology/api2/PostTest.java
@@ -0,0 +1,25 @@
+package ru.netology.api2;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+void  PostTestSuccess () {
+    // Given - When - Then
+// Предусловия
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200)
+    .body("data", equalTo("some value"));
+
+    }
+}
